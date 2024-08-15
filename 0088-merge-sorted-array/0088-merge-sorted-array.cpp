8bd03aae61bf8462134e8aa922c0cc62df8b632b
Pranshu Sharma <pranshu.sharma1303@gmail.com>
2024-08-15 18:32:55 +0530

    Time: 0 ms (100%), Space: 11 MB (88.29%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> ans;
+        int i=0,j=0;
+
+        // input all the values in ans until unless 
+        // all elements of one array are inserted.
+        while(i < m && j < n)
+        {
+            if(nums1[i] <= nums2[j])
+                ans.push_back(nums1[i++]);
+            else if(nums2[j] <= nums1[i])
+                ans.push_back(nums2[j++]);
+        }
+
+        // // Now store the remaining elements of array 1.
+        while(i<m)
+            ans.push_back(nums1[i++]);
+        
+        // Now store the remaining elements of array 2.
+        while(j<n)
+            ans.push_back(nums2[j++]);
+        
+        nums1 = ans;
+
+    }
+};
