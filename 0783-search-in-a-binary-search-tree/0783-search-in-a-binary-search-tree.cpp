ca46469658704d7836b5387919afcee1c8978e2f
Pranshu Sharma <pranshu.sharma1303@gmail.com>
2024-12-11 19:32:29 +0530

    Time: 0 ms (100%), Space: 35.4 MB (17.12%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.cpp
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.cpp
@@ -11,20 +11,17 @@
  */
 class Solution {
 public:
-    TreeNode* search(TreeNode* root, int val) {
-    // Base case
-    if(root == NULL || root->val == val) {
-        return root; // Return root if NULL or value matches.
-    }
-
-    // Recursively search left or right subtree based on comparison.
-    return (val < root->val) ? search(root->left, val) : search(root->right, val);
-    }
-
     TreeNode* searchBST(TreeNode* root, int val) {
-        // Wrapper function that calls the recursive `search` function.
-        // Returns the result of the search for the target value in the BST.
-        return search(root, val);
+        // iterative approach
+        TreeNode* temp = root;
+        // search for the value
+        while(temp != NULL){
+            // val found
+            if(temp->val == val) return temp;
+            else if(temp->val < val) temp = temp->right;
+            else temp = temp->left;
+        }
+        // not found val
+        return NULL;
     }
-
 };
