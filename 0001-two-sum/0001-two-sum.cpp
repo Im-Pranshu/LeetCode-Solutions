b4acca936b30c57c4affaa9772553fea67c6dbec
Pranshu Sharma <pranshu.sharma1303@gmail.com>
2025-01-30 20:03:51 +0530

    Time: 3 ms (74.63%), Space: 15.1 MB (12.36%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
-    // Function to find two indices such that their values add up to the target
-vector<int> twoSum(vector<int>& nums, int target) {
-    // Create a hash map to store value-to-index mappings
-    unordered_map<int, int> mp;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mp;
 
-    // Iterate through the array
-    for (int i = 0; i < nums.size(); i++) {
-        
-        // Calculate the complement that would sum with nums[i] to reach the target
-        int complement = target - nums[i];
-
-        // Check if the complement is already in the map (i.e., it was seen in a previous iteration)
-        if (mp.find(complement) != mp.end()) {
-            // If found, return the indices of the current value and the value that was previously stored
-            return {i, mp[complement]};
-        } else {
-            // Otherwise, store the current value and its index in the map
-            // This will help in finding its complement in future iterations
-            mp[nums[i]] = i;
+        // puri array ko check karo agar map hai to warna dal do map me
+        for(int i=0; i<nums.size(); i++){
+            // iska jo difference hai wo array me hoga to usko check karlo
+            int complement = target - nums[i];
+            
+            // pure map me search karo agar hai to dono ke index ko return kardo
+            if(mp.find(target - nums[i]) != mp.end())
+                return {i, mp[target - nums[i]]};
+            // warna index ko store kardo
+            else
+                mp[nums[i]] = i;
         }
-    }
 
-    // If no pair is found that adds up to the target, return {-1, -1}
-    return {-1, -1};
-}
+        return {-1,-1};
+    }
 };
