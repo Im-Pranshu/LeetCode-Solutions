f7276e1a34c4a08bbb27eacb347b4327024a3e72
Pranshu Sharma <pranshu.sharma1303@gmail.com>
2025-03-19 21:45:12 +0530

    Time: 4 ms (60.18%), Space: 14.9 MB (21.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        map<int,int> mp;
+        map<int, int> mp;
 
-        // puri array ko check karo agar map hai to warna dal do map me
         for(int i=0; i<nums.size(); i++){
-            // iska jo difference hai wo array me hoga to usko check karlo
             int complement = target - nums[i];
+
+            if(mp.find(complement) != mp.end()){
+                return {i, mp[complement]};
+            }
             
-            // pure map me search karo agar hai to dono ke index ko return kardo
-            if(mp.find(target - nums[i]) != mp.end())
-                return {i, mp[target - nums[i]]};
-            // warna index ko store kardo
-            else
-                mp[nums[i]] = i;
+            // map me index store karlo kuki index hi return karna hai
+            mp[nums[i]] = i;
+            
         }
 
         return {-1,-1};
