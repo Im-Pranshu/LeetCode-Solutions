d597a5e9fb0a94315e52dc2b118cf65d74ac32ba
Pranshu Sharma <pranshu.sharma1303@gmail.com>
2025-03-19 21:33:38 +0530

    Time: 55 ms (66.99%), Space: 54.3 MB (13.03%) - LeetHub

--- 2859-add-two-promises/2859-add-two-promises.js
+++ 2859-add-two-promises/2859-add-two-promises.js
@@ -5,7 +5,8 @@
  */
 var addTwoPromises = async function(promise1, promise2) {
     // promise.all() resolve all promises together and return resolve if all are resolved else if any one rejects it'll reject the promise.
-    const [val1, val2] = await Promise.all([promise1, promise2])
+    const val1 = await promise1;
+    const val2 = await promise2;
     
     return val1 + val2
 };
