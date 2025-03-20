8167ea01a45d2d22d11f5aa4b35e6b94f6c791ed
Pranshu Sharma <pranshu.sharma1303@gmail.com>
2025-03-20 22:09:03 +0530

    Time: 41 ms (82.89%), Space: 53.3 MB (26.77%) - LeetHub

---
+++ 2733-sleep/2733-sleep.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+    // return a promise and resolve it after millis
+    return new Promise((resolve) => setTimeout(resolve, millis))
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
