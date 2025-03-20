16d00b69d28cfc9820490ea002c5d06e160773b7
Pranshu Sharma <pranshu.sharma1303@gmail.com>
2025-03-20 22:25:19 +0530

    Time: 43 ms (27.08%), Space: 75.7 MB (17.6%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mp;
 
-        // O(nLog(n)) -> basic approach
-        sort(nums.begin(), nums.end());
+        // store the occurence in the map 
+        for(auto it: nums){
+            mp[it]++;
+        }
 
-        for(int i=0; i<nums.size()-1; i++){
-            if (nums[i] == nums[i+1]){
-                return true;
+        // // if occurence is more than once then duplicate exist
+        // for(int i=0; i<mp.size(); i++){
+        //     if(mp[i] > 1){
+        //         return 1;
+        //     }
+        // }
+        for (auto& it : mp) {
+            if (it.second > 1) {
+                return true; // Duplicate exists
             }
         }
-        return false;
+        return 0;
     }
 };
