91bf638dcf649d70e9531902538d39b0f3e42b53
Pranshu Sharma <pranshu.sharma1303@gmail.com>
2025-03-21 22:43:32 +0530

    Time: 49 ms (89.72%), Space: 54.3 MB (17.05%) - LeetHub

---
+++ 2821-timeout-cancellation/2821-timeout-cancellation.js
@@ -0,0 +1,46 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+    
+    // clearTimout will be called if t < cancelTimeMs(in this time cancel fn will be called)
+    const cancelFn = () => {
+        clearTimeout(timer)
+    }
+
+
+    const timer = setTimeout(() => {
+            // call the fn with the following args
+            fn(...args)
+    }, t)
+
+    // return the cancelFn - if t is less then clearTimeout will be called and fn will not be called.
+    return cancelFn
+};
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 5;
+ *  const args = [2], t = 20, cancelTimeMs = 50;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  const maxT = Math.max(t, cancelTimeMs);
+ *           
+ *  setTimeout(cancel, cancelTimeMs);
+ *
+ *  setTimeout(() => {
+ *      console.log(result); // [{"time":20,"returned":10}]
+ *  }, maxT + 15)
+ */
